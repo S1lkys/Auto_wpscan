ef575af04249c7c2af3a5763004a34a5cd8e25ac
Silky <40408435+S1lkys@users.noreply.github.com>
2019-03-11 18:52:08 +0100

    Add files via upload

---
+++ auto_wpscan.sh
@@ -0,0 +1,10 @@
+wpscan --update
+read -p $'\e[1;92m Liste mit Wordpresswebistes: \e[0m' wl_pass 
+wl_pass="${wl_pass}"
+
+count_pass=$(wc -l $wl_pass | cut -d " " -f1)
+
+for fn in `cat $wl_pass`; do
+wpscan --url $fn -e  u,vp 
+done
+
